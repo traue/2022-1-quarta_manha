18e952f9311af1e19ce5c1bd833c1e867a73c71d
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-02-09 11:27:43 -0300

    Nosso primeiro projeto - Hello World

--- Projeto Hello World/Projeto Hello World/Form1.Designer.cs
+++ Projeto Hello World/Projeto Hello World/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(216, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(295, 129);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clique-me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(754, 390);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Projeto Hello World/Projeto Hello World/Form1.cs
+++ Projeto Hello World/Projeto Hello World/Form1.cs
@@ -16,5 +16,10 @@ namespace Projeto_Hello_World
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World! :D Funcionou!");
+        }
     }
 }
