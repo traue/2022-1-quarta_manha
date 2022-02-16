cc57ad70b88612b1b1825860bfae2178988dd303
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-02-16 11:31:03 -0300

    Primeiro Projeto Winforms

---
+++ Primeiro Projeto Winforms/Primeiro Projeto Winforms/FrmPrincipal.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Primeiro_Projeto_Winforms
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbNome
+            // 
+            this.lbNome.AutoSize = true;
+            this.lbNome.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNome.Location = new System.Drawing.Point(12, 99);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(281, 36);
+            this.lbNome.TabIndex = 0;
+            this.lbNome.Text = "Informe seu nome:";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 582);
+            this.Controls.Add(this.lbNome);
+            this.Name = "FrmPrincipal";
+            this.Text = "Meu Primeiro Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNome;
+    }
+}
+
