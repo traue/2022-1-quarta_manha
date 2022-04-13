00f02d1feaaa505fcdc05edb21a28b332087893d
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-04-13 10:19:06 -0300

    Criação do projeto

---
+++ Projeto Viagem/Projeto Viagem/FrmPrincipal.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Projeto_Viagem
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.lbDistancia = new System.Windows.Forms.Label();
+            this.lbConsumo = new System.Windows.Forms.Label();
+            this.lbCombustivel = new System.Windows.Forms.Label();
+            this.lbPedagio = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lbDistancia
+            // 
+            this.lbDistancia.AutoSize = true;
+            this.lbDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDistancia.Location = new System.Drawing.Point(12, 76);
+            this.lbDistancia.Name = "lbDistancia";
+            this.lbDistancia.Size = new System.Drawing.Size(324, 37);
+            this.lbDistancia.TabIndex = 0;
+            this.lbDistancia.Text = "Distância a percorrer:";
+            // 
+            // lbConsumo
+            // 
+            this.lbConsumo.AutoSize = true;
+            this.lbConsumo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbConsumo.Location = new System.Drawing.Point(12, 189);
+            this.lbConsumo.Name = "lbConsumo";
+            this.lbConsumo.Size = new System.Drawing.Size(412, 37);
+            this.lbConsumo.TabIndex = 1;
+            this.lbConsumo.Text = "Consumo médio do veículo:";
+            // 
+            // lbCombustivel
+            // 
+            this.lbCombustivel.AutoSize = true;
+            this.lbCombustivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCombustivel.Location = new System.Drawing.Point(12, 297);
+            this.lbCombustivel.Name = "lbCombustivel";
+            this.lbCombustivel.Size = new System.Drawing.Size(373, 37);
+            this.lbCombustivel.TabIndex = 2;
+            this.lbCombustivel.Text = "Valor do combustível: R$";
+            // 
+            // lbPedagio
+            // 
+            this.lbPedagio.AutoSize = true;
+            this.lbPedagio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPedagio.Location = new System.Drawing.Point(12, 487);
+            this.lbPedagio.Name = "lbPedagio";
+            this.lbPedagio.Size = new System.Drawing.Size(448, 37);
+            this.lbPedagio.TabIndex = 3;
+            this.lbPedagio.Text = "Valor gasto com pedágios: R$";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(23, 388);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(437, 41);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Houve gastos com pedágio";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 758);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lbPedagio);
+            this.Controls.Add(this.lbCombustivel);
+            this.Controls.Add(this.lbConsumo);
+            this.Controls.Add(this.lbDistancia);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gasto da Viagem";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbDistancia;
+        private System.Windows.Forms.Label lbConsumo;
+        private System.Windows.Forms.Label lbCombustivel;
+        private System.Windows.Forms.Label lbPedagio;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
