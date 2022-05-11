e897fb983420e43f6a8dc3302e90447ad7a4596e
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-05-11 09:58:14 -0300

    Uma possível solução do exercício

---
+++ Solução - Exercicio Caixa Eletronico/Solução - Exercicio Caixa Eletronico/FrmCaixa.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Solução___Exercicio_Caixa_Eletronico
+{
+    partial class FrmCaixa
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValorSaqui = new System.Windows.Forms.TextBox();
+            this.btSacar = new System.Windows.Forms.Button();
+            this.lbNotasNecessarias = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor do Saque:";
+            // 
+            // txtValorSaqui
+            // 
+            this.txtValorSaqui.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValorSaqui.Location = new System.Drawing.Point(308, 82);
+            this.txtValorSaqui.Name = "txtValorSaqui";
+            this.txtValorSaqui.Size = new System.Drawing.Size(188, 50);
+            this.txtValorSaqui.TabIndex = 1;
+            // 
+            // btSacar
+            // 
+            this.btSacar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSacar.Location = new System.Drawing.Point(20, 156);
+            this.btSacar.Name = "btSacar";
+            this.btSacar.Size = new System.Drawing.Size(476, 73);
+            this.btSacar.TabIndex = 2;
+            this.btSacar.Text = "Sacar";
+            this.btSacar.UseVisualStyleBackColor = true;
+            this.btSacar.Click += new System.EventHandler(this.btSacar_Click);
+            // 
+            // lbNotasNecessarias
+            // 
+            this.lbNotasNecessarias.AutoSize = true;
+            this.lbNotasNecessarias.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNotasNecessarias.Location = new System.Drawing.Point(15, 284);
+            this.lbNotasNecessarias.Name = "lbNotasNecessarias";
+            this.lbNotasNecessarias.Size = new System.Drawing.Size(58, 44);
+            this.lbNotasNecessarias.TabIndex = 3;
+            this.lbNotasNecessarias.Text = "---";
+            // 
+            // FrmCaixa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 804);
+            this.Controls.Add(this.lbNotasNecessarias);
+            this.Controls.Add(this.btSacar);
+            this.Controls.Add(this.txtValorSaqui);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCaixa";
+            this.Text = "Super Caixa Eletrônico";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValorSaqui;
+        private System.Windows.Forms.Button btSacar;
+        private System.Windows.Forms.Label lbNotasNecessarias;
+    }
+}
+
---
+++ Solução - Exercicio Caixa Eletronico/Solução - Exercicio Caixa Eletronico/FrmCaixa.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solução___Exercicio_Caixa_Eletronico
+{
+    public partial class FrmCaixa : Form
+    {
+        public FrmCaixa()
+        {
+            InitializeComponent();
+        }
+
+        private void btSacar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lbNotasNecessarias.Text = calculaNotasNecessarias(int.Parse(txtValorSaqui.Text));
+            }
+            catch
+            {
+                MessageBox.Show("Ops... algo deu errado...");
+            }
+        }
+
+        private String calculaNotasNecessarias(int valorSaque)
+        {
+            string notasNecessarias = "";
+
+            int[] notas = { 100, 50, 20, 10, 5, 2 };
+            int resultado = 0;
+            int divide = 0;
+
+            if (valorSaque == 1 || valorSaque == 3)
+            {
+                notasNecessarias = "Valor impossível de ser sacado!";
+            }
+            else
+            {
+                foreach(int nota in notas)
+                {
+                    if(valorSaque >= nota)
+                    {
+                        resultado = valorSaque % nota;
+                        divide = valorSaque / nota;
+
+                        if(resultado != 3 && resultado != 1)
+                        {
+                            notasNecessarias += divide + " nota(s) de " + nota + Environment.NewLine;
+                            valorSaque -= (nota * divide);
+                        }
+
+                        if(nota >= 10 && resultado == 3)
+                        {
+                            valorSaque -= 5;
+                            notasNecessarias += "1 nota de 5" + Environment.NewLine;
+                        }
+
+                        if(nota >= 10 && resultado == 1)
+                        {
+                            valorSaque -= 6;
+                            notasNecessarias += "3 notas de 2" + Environment.NewLine;
+                        }
+                    }
+                }
+            }
+
+            return notasNecessarias;
+        }
+    }
+}
