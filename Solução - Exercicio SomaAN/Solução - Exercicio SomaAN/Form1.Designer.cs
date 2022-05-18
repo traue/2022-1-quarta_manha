864a7cd2d6cf3215a7e392fc6c430ce2ea5a06b0
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-05-18 09:57:59 -0300

    Solução do exercício

---
+++ Solução - Exercicio SomaAN/Solução - Exercicio SomaAN/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Solução___Exercicio_SomaAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbResposta = new System.Windows.Forms.Label();
+            this.txt_va = new System.Windows.Forms.TextBox();
+            this.txt_vn = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCalcular.Location = new System.Drawing.Point(20, 220);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(290, 75);
+            this.btCalcular.TabIndex = 0;
+            this.btCalcular.Text = "Soma(a, N)";
+            this.btCalcular.UseVisualStyleBackColor = true;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Valor \"a\":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 44);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor \"N\": ";
+            // 
+            // lbResposta
+            // 
+            this.lbResposta.AutoSize = true;
+            this.lbResposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResposta.Location = new System.Drawing.Point(12, 332);
+            this.lbResposta.Name = "lbResposta";
+            this.lbResposta.Size = new System.Drawing.Size(84, 44);
+            this.lbResposta.TabIndex = 3;
+            this.lbResposta.Text = "-----";
+            // 
+            // txt_va
+            // 
+            this.txt_va.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_va.Location = new System.Drawing.Point(210, 50);
+            this.txt_va.Name = "txt_va";
+            this.txt_va.Size = new System.Drawing.Size(100, 50);
+            this.txt_va.TabIndex = 4;
+            // 
+            // txt_vn
+            // 
+            this.txt_vn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_vn.Location = new System.Drawing.Point(210, 145);
+            this.txt_vn.Name = "txt_vn";
+            this.txt_vn.Size = new System.Drawing.Size(100, 50);
+            this.txt_vn.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_vn);
+            this.Controls.Add(this.txt_va);
+            this.Controls.Add(this.lbResposta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCalcular);
+            this.Name = "Form1";
+            this.Text = "SomaAN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbResposta;
+        private System.Windows.Forms.TextBox txt_va;
+        private System.Windows.Forms.TextBox txt_vn;
+    }
+}
+
---
+++ Solução - Exercicio SomaAN/Solução - Exercicio SomaAN/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solução___Exercicio_SomaAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            int a, n;
+            Int32.TryParse(txt_va.Text, out a);
+            Int32.TryParse(txt_vn.Text, out n);
+            lbResposta.Text = SomaAN(a, n);
+        }
+
+        private String SomaAN(int a, int n)
+        {
+            int contador = a;
+            int aux = a;
+            String saida = a.ToString();
+            for(int i = 1; i < n; i++)
+            {
+                contador += (a + i);
+                saida += " + ";
+                aux = a + i;
+                saida += aux;
+            }
+
+            return saida + " = " + contador;
+        }
+    }
+}
